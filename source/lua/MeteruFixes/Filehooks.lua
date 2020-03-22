18ce2b076d04951575abfac29733c96e75806c81
diego <1570625+ailmanki@users.noreply.github.com>
2020-03-22 16:15:51 +0100

    fix for map blib error

--- source/lua/MeteruFixes/Filehooks.lua
+++ source/lua/MeteruFixes/Filehooks.lua
@@ -1,5 +1,7 @@
 ModLoader.SetupFileHook( "lua/MAC.lua", "lua/MeteruFixes/MAC.lua", "replace" )
+ModLoader.SetupFileHook( "lua/MapBlipMixin.lua", "lua/MeteruFixes/MapBlipMixin.lua", "replace" )
 ModLoader.SetupFileHook( "lua/Player.lua", "lua/MeteruFixes/Player.lua", "replace" )
 ModLoader.SetupFileHook( "lua/Weapons/Alien/Shockwave.lua", "lua/MeteruFixes/Weapons/Alien/Shockwave.lua", "replace" )
 ModLoader.SetupFileHook( "lua/Weapons/Marine/Flamethrower.lua", "lua/MeteruFixes/Weapons/Marine/Flamethrower.lua", "replace" )
 ModLoader.SetupFileHook( "lua/Weapons/Marine/Minigun.lua", "lua/MeteruFixes/Weapons/Marine/Minigun.lua", "replace" )
+
